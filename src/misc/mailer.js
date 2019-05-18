cc5a29fe198934641c9fe3fb53a37669b11afe11
tundeUsman <tundeusman0@gmail.com>
2019-05-18 12:24:15 +0100

    added heroku mailGun addon

--- src/misc/mailer.js
+++ src/misc/mailer.js
@@ -7,8 +7,8 @@ import mailgunTransport from 'nodemailer-mailgun-transport'
 // Configure transport options
 const mailgunOptions = {
     auth: {
-        api_key: "16c799ad50f469636b15fcfb12d253d7-4a62b8e8-859bb623",
-        domain: "sandbox81c1e889e5ed460d8d97442bdfc1fc44.mailgun.org" || "https://gentle-waters-83618.herokuapp.com/",
+        api_key: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN,
     }
 }
 const transport = mailgunTransport(mailgunOptions)
